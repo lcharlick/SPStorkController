47713016bc6f14db2d33bc86234d772bb95b0a60
Lachlan Charlick <lachlan.charlick@gmail.com>
2019-09-26 16:25:29 +0930

    Support for dynamically disabling dismiss pan gesture

--- Source/SPStorkController/TransitioningDelegate/SPStorkPresentationController.swift
+++ Source/SPStorkController/TransitioningDelegate/SPStorkPresentationController.swift
@@ -21,7 +21,7 @@
 
 import UIKit
 
-class SPStorkPresentationController: UIPresentationController, UIGestureRecognizerDelegate {
+public class SPStorkPresentationController: UIPresentationController, UIGestureRecognizerDelegate {
     
     var swipeToDismissEnabled: Bool = true
     var tapAroundToDismissEnabled: Bool = true
@@ -39,6 +39,13 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
     weak var confirmDelegate: SPStorkControllerConfirmDelegate?
     
     var pan: UIPanGestureRecognizer?
+
+    public var isSwipeToDismissEnabled: Bool = true {
+        didSet {
+            self.pan?.isEnabled = self.isSwipeToDismissEnabled
+        }
+    }
+
     var tap: UITapGestureRecognizer?
     
     private var closeButton = SPStorkCloseButton()
@@ -73,7 +80,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
     
     private var feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
     
-    override var presentedView: UIView? {
+    override public var presentedView: UIView? {
         let view = self.presentedViewController.view
         if view?.frame.origin == CGPoint.zero {
             view?.frame = self.frameOfPresentedViewInContainerView
@@ -81,7 +88,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
         return view
     }
     
-    override var frameOfPresentedViewInContainerView: CGRect {
+    override public var frameOfPresentedViewInContainerView: CGRect {
         guard let containerView = containerView else { return .zero }
         let baseY: CGFloat = self.topSpace + 13
         let maxHeight: CGFloat = containerView.bounds.height - baseY
@@ -97,7 +104,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
         return CGRect(x: 0, y: containerView.bounds.height - height, width: containerView.bounds.width, height: height)
     }
     
-    override func presentationTransitionWillBegin() {
+    override public func presentationTransitionWillBegin() {
         super.presentationTransitionWillBegin()
         
         if !self.hapticMoments.isEmpty {
@@ -218,7 +225,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
         }
     }
     
-    override func presentationTransitionDidEnd(_ completed: Bool) {
+    override public func presentationTransitionDidEnd(_ completed: Bool) {
         super.presentationTransitionDidEnd(completed)
         guard let containerView = containerView else { return }
         self.updateSnapshot()
@@ -243,7 +250,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
         }
     }
     
-    override func dismissalTransitionWillBegin() {
+    override public func dismissalTransitionWillBegin() {
         super.dismissalTransitionWillBegin()
         guard let containerView = containerView else { return }
         self.startDismissing = true
@@ -308,7 +315,7 @@ class SPStorkPresentationController: UIPresentationController, UIGestureRecogniz
         })
     }
     
-    override func dismissalTransitionDidEnd(_ completed: Bool) {
+    override public func dismissalTransitionDidEnd(_ completed: Bool) {
         super.dismissalTransitionDidEnd(completed)
         guard let containerView = containerView else { return }
         
@@ -426,7 +433,7 @@ extension SPStorkPresentationController {
         }
     }
     
-    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         if let gester = gestureRecognizer as? UIPanGestureRecognizer {
             let velocity = gester.velocity(in: self.presentedViewController.view)
             return abs(velocity.y) > abs(velocity.x)
@@ -508,7 +515,7 @@ extension SPStorkPresentationController {
 
 extension SPStorkPresentationController {
     
-    override func containerViewWillLayoutSubviews() {
+    override public func containerViewWillLayoutSubviews() {
         super.containerViewWillLayoutSubviews()
         guard let containerView = containerView else { return }
         self.updateSnapshotAspectRatio()
@@ -517,7 +524,7 @@ extension SPStorkPresentationController {
         }
     }
     
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    override public func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(alongsideTransition: { contex in
             self.updateLayoutIndicator()
